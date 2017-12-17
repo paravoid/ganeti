017db757b32a495d1a2e862fdd88a9b35c7aee28
Apollon Oikonomopoulos <apoikos@debian.org>
2017-12-17 14:07:57 +0200

    netutils: use SO_PEERCRED from socket
    It's available in Python 3

--- lib/netutils.py
+++ lib/netutils.py
@@ -41,7 +41,6 @@ import os
 import re
 import socket
 import struct
-import IN
 import logging
 
 from ganeti import constants
@@ -64,7 +63,7 @@ _STRUCT_UCRED_SIZE = struct.calcsize(_STRUCT_UCRED)
 # Workaround a bug in some linux distributions that don't define SO_PEERCRED
 try:
   # pylint: disable=E1101
-  _SO_PEERCRED = IN.SO_PEERCRED
+  _SO_PEERCRED = socket.SO_PEERCRED
 except AttributeError:
   _SO_PEERCRED = 17
 
